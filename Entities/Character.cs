629b82de1c4e0a6e79b7802999fbb838320d01de
maritoFdz <mariofdez314@gmail.com>
2025-07-04 19:37:33 -0500

    Add Characters class in Entities folder
    Implement abstract class for characters general behavior

---
+++ Entities/Character.cs
@@ -0,0 +1,24 @@
+﻿namespace Gwynbleidd.Entities;
+
+public abstract class Character
+{
+    public readonly int Velocity;
+    public readonly int SkillCooldown;
+    public int VelocityModifier { get; protected set; }
+    public bool CantMove { get; protected set; }
+    public bool HasCiri { get; protected set; }
+
+    public Character(int velocity, int SkillCooldown)
+    {
+        this.Velocity = velocity;
+        this.SkillCooldown = SkillCooldown;
+    }
+
+    public void Move(int direction)
+    {
+        int realVelocity = Velocity + VelocityModifier;
+        // TODO WHEN BOARD IS IMPLEMENTED
+    }
+
+    public abstract void UseSkill();
+}
