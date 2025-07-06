804fe770d899eb20b01a222060130cd4701bd66b
maritoFdz <mariofdez314@gmail.com>
2025-07-06 12:27:02 -0500

    Change in Squares classes
    Eliminated constructor (everything is false by default) and trap now is not an obstacle

--- Maze/BoardSquare.cs
+++ Maze/BoardSquare.cs
@@ -4,11 +4,4 @@ public class BoardSquare
     public bool IsOccupied { get; set; }
     public bool IsFrozen { get; set; }
     public bool IsObstacle { get; set; }
-
-    public BoardSquare(bool isOccupied = false, bool isFrozen = false, bool isObstacle = false)
-    {
-        IsOccupied = isOccupied;
-        IsFrozen = isFrozen;
-        IsObstacle = isObstacle;
-    }
 }
--- Maze/Obstacles/Trap.cs
+++ Maze/Obstacles/Trap.cs
@@ -1,11 +1,7 @@
 ﻿namespace Gwynbleidd.Maze.Obstacles;
 public abstract class Trap : BoardSquare
 {
-    private const bool isObstacle = true;
-    private const bool isFrozen = false;
-    private const bool isOccupied = false;
-
-    public Trap() : base(isOccupied, isFrozen, isObstacle) { }
+    public Trap() : base() { }
 
     public abstract void TriggerTrap();
 }
