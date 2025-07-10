5e7bc47ecd4d1d0be976da57409bea65982cd721
maritoFdz <mariofdez314@gmail.com>
2025-07-10 16:45:23 -0500

    Change Item class name

--- Entities/Potion.cs
+++ Entities/Potion.cs
@@ -2,14 +2,15 @@
 
 namespace Gwynbleidd.Entities;
 
-public class Item(string name, int amountOfTurns, int vMod, int cMod) : IModifier
+public class Potion(string name, int amountOfTurns, int vMod, int cMod) : IModifier
 {
-    public string Name { get; private set; } = name;
+    public string Name { get; protected set; } = name;
     public int RemainingTurns { get; private set; } = amountOfTurns;
     public Character? Target { get; private set; } = null;
-    public (int X, int Y) Position { get; private set; }
     public int VelocityModifier { get; private set; } = vMod;
     public int CooldownModifier { get; private set; } = cMod;
+    public (int X, int Y) Position { get; private set; }
+    public string? Appareance { get; private set; }
 
 
     public void OnTurnEnd()
